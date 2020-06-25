ed3bdeb93a8b304b294be4277727894f073674fb
madumas <madumas@madumas.ca>
2020-06-25 16:13:26 -0400

    apply errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const txDecoder = require('ethereum-tx-decoder');
 const web3 = require('web3');
 
-var argv = require('yargs')
+let argv = require('yargs')
     .option('upstream', {
         alias: 'u',
         describe: 'HTTP URI for Ethereum node to which this service should proxy requests',
@@ -45,22 +45,14 @@ var argv = require('yargs')
 ;
 
 // validate commandline arguments
-console.assert(argv.txErrorRate >= 0 && argv.txErrorRate < 1, "Please specify txErrorRate between 0.0 and 1.0")
-console.assert(argv.minGas >= 0, "Please specify minGas greater than 0")
-console.assert(argv.delay >= 0, "Cannot simulate shorter-than-normal latencies")
-console.assert(argv.rpcErrorRate >= 0 && argv.rpcErrorRate < 1, "Please specify rpcErrorRate between 0.0 and 1.0")
+console.assert(argv.txErrorRate >= 0 && argv.txErrorRate < 1, "Please specify txErrorRate between 0.0 and 1.0");
+console.assert(argv.minGas >= 0, "Please specify minGas greater than 0");
+console.assert(argv.delay >= 0, "Cannot simulate shorter-than-normal latencies");
+console.assert(argv.rpcErrorRate >= 0 && argv.rpcErrorRate < 1, "Please specify rpcErrorRate between 0.0 and 1.0");
 
 const app = express();
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-const config = {
-  server: {
-    host: argv.host,
-    port: argv.port
-  },
-  upstream: argv.upstream
-}
 
 app.use((req, res, next) => {
   req.setEncoding('utf8');
@@ -69,14 +61,12 @@ app.use((req, res, next) => {
   req.on('end', () => next());
 });
 
-http.createServer(app).listen(config.server.port, config.server.host);
-console.log("Waiting for connections on " + config.server.host + ":" + config.server.port)
+http.createServer(app).listen(argv.port, argv.host);
+console.log("Waiting for connections on " + argv.host + ":" + argv.port);
 
-app.post('/', function (request, response, next)
+app.post('/', function (request, response, )
 {
   let rpc;
-
-  console.log(request.rpc)
 
   try {
     rpc = JSON.parse(request.rpc);
@@ -87,35 +77,48 @@ app.post('/', function (request, response, next)
     });
   }
 
-  if(rpc.method==='eth_sendRawTransaction') {
-    const decodedData =  txDecoder.decodeTx(rpc.params[0]);
-    const txhash = web3.utils.keccak256(rpc.params[0])
-    console.log(decodedData);
-
-    if(Math.floor(Math.random()*2)===0) {
-      //Drop 1 tx of 2
-      console.log('Dropped transaction ' + txhash)
-      response.status(200).send(JSON.stringify({
+  setTimeout(function(){
+    if(Math.random() < argv.rpcErrorRate){
+      console.log('Injecting error on ' + request.rpc);
+      response.status(500).send(JSON.stringify({
         jsonrpc: '2.0',
-        result: web3.utils.keccak256(rpc.params[0])
-      }))
+        error: {
+          code: -32000,
+          message: 'Ethereum Monkey Injected Error'
+        },
+        id: rpc.id
+      }));
       return;
     }
-  }
 
-  axios.post(config.upstream, request.rpc).then((res) => {
-    let responseData = res.data;
-    response.status(res.status).send(responseData);
-    console.log('resp:'+JSON.stringify(responseData))
-  }).catch(e => {
-    console.log(e)
-    response.status(e.response.status).send(e.response.data);
-  });
+    if (rpc.method === 'eth_sendRawTransaction') {
+      const decodedData = txDecoder.decodeTx(rpc.params[0]);
+      const txHash = web3.utils.keccak256(rpc.params[0]);
+
+      if (Math.random() < argv.txErrorRate || decodedData.gasPrice.toNumber() < argv.minGas * 1E9) {
+        console.log('Dropped transaction ' + txHash);
+        response.status(200).send(JSON.stringify({
+          jsonrpc: '2.0',
+          result: txHash,
+          id: rpc.id
+        }));
+        return;
+      }
+    }
+
+    axios.post(argv.upstream, request.rpc).then((res) => {
+      let responseData = res.data;
+      response.status(res.status).send(responseData);
+    }).catch(e => {
+      console.log(e);
+      response.status(e.response.status).send(e.response.data);
+    });
+  },argv.delay);
 });
 
 
 
-app.use(function(req, res, next) {
+app.use(function(req, res, ) {
   return res.status(404).send({
     code: -32601,
     message: "Not found"
