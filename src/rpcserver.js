66d07ff2155d09976d57d76fef7f38f5e7851645
madumas <madumas@madumas.ca>
2020-06-26 12:22:29 -0400

    Queue transactions that follow a dropped or queued nonce

--- src/rpcserver.js
+++ src/rpcserver.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
 const axios = require('axios');
-const txDecoder = require('ethereum-tx-decoder');
+const ethers = require('ethers');
 const web3 = require('web3');
 const app = express();
 const httpTerminator = require('http-terminator');
@@ -9,10 +9,76 @@ const httpTerminator = require('http-terminator');
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 let config, server;
+let skippedTx={};
+
+const biggestSkippedNonce = function(addr) {
+  let nonce=-1;
+  if (skippedTx[addr]) {
+    nonce = skippedTx[addr].length-1;
+  }
+  return nonce
+};
 
 function RpcServer(conf) {
   config = conf;
 }
+
+async function sendTransaction(request,response,signedTx) {
+  if(request&&response) {
+    const res=await axios.post(config.upstream, request.rpc).catch(e => {
+      console.log(e);
+      response.status(e.response.status).send(e.response.data);
+    });
+    let responseData = res.data;
+    response.status(res.status).send(responseData);
+  } else {
+    await axios.post(config.upstream, JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'eth_sendRawTransaction',
+      params: signedTx,
+      id: 1
+    })).catch(e => {
+      console.log(e);
+    });
+  }
+}
+
+function processSendTransaction(rawTx,request,response,rpc) {
+  const decodedData = ethers.utils.parseTransaction(rawTx);
+  const txHash = web3.utils.keccak256(rawTx);
+  if (Math.random() < config.txErrorRate
+    || decodedData.gasPrice.toNumber() < config.minGas * 1E9
+    || (biggestSkippedNonce(decodedData.from)!==-1 &&decodedData.nonce>biggestSkippedNonce(decodedData.from))
+  ) {
+    console.log('Queued transaction with Nonce ' + decodedData.nonce);
+    skippedTx[decodedData.from]=skippedTx[decodedData.from]||[];
+    skippedTx[decodedData.from][decodedData.nonce]=rawTx;
+    response.status(200).send(JSON.stringify({
+      jsonrpc: '2.0',
+      result: txHash,
+      id: rpc.id
+    }));
+  } else {
+    if (skippedTx[decodedData.from] && skippedTx[decodedData.from][decodedData.nonce]) {
+      //this is a new tx for an existing transaction
+      sendTransaction(request,response,skippedTx[decodedData.from][decodedData.nonce]).then(()=> {
+        delete (skippedTx[decodedData.from][decodedData.nonce]);
+        //check if there are others in queue and process them
+        let queuedTx;
+        while (skippedTx[decodedData.from].length > 0) {
+          if ((queuedTx = skippedTx[decodedData.from].shift()) !== undefined) {
+            console.log('Reprocessing: ' + queuedTx);
+            processSendTransaction(queuedTx, null, null, null);
+          }
+        }
+      });
+
+    } else {
+      sendTransaction(request,response,rawTx).then();
+    }
+  }
+}
+
 
 RpcServer.prototype.start= function() {
 
@@ -53,27 +119,18 @@ RpcServer.prototype.start= function() {
       }
 
       if (rpc.method === 'eth_sendRawTransaction') {
-        const decodedData = txDecoder.decodeTx(rpc.params[0]);
-        const txHash = web3.utils.keccak256(rpc.params[0]);
 
-        if (Math.random() < config.txErrorRate || decodedData.gasPrice.toNumber() < config.minGas * 1E9) {
-          console.log('Dropped transaction ' + txHash);
-          response.status(200).send(JSON.stringify({
-            jsonrpc: '2.0',
-            result: txHash,
-            id: rpc.id
-          }));
-          return;
-        }
+        processSendTransaction(rpc.params[0],request,response,rpc);
+
+      } else {
+        axios.post(config.upstream, request.rpc).then((res) => {
+          let responseData = res.data;
+          response.status(res.status).send(responseData);
+        }).catch(e => {
+          console.log(e);
+          response.status(e.response.status).send(e.response.data);
+        });
       }
-
-      axios.post(config.upstream, request.rpc).then((res) => {
-        let responseData = res.data;
-        response.status(res.status).send(responseData);
-      }).catch(e => {
-        console.log(e);
-        response.status(e.response.status).send(e.response.data);
-      });
     }, config.delay);
   });
 
--- tests/nonce.test.js
+++ tests/nonce.test.js
@@ -7,7 +7,7 @@ const Web3 = require('web3');
 const bip39 = require('bip39');
 const {hdkey} = require('ethereumjs-wallet');
 const testchainMnemonic = "hill law jazz limb penalty escape public dish stand bracket blue jar";
-const EthereumTx = require('ethereumjs-tx').Transaction
+const EthereumTx = require('ethereumjs-tx').Transaction;
 
 const seed = bip39.mnemonicToSeedSync(testchainMnemonic); // mnemonic is the string containing the words
 const hdk = hdkey.fromMasterSeed(seed);
@@ -15,7 +15,7 @@ const addr_node = hdk.derivePath("m/44'/60'/0'/0/0"); //m/44'/60'/0'/0/0 is deri
 const ethFrom = addr_node.getWallet().getAddressString(); //check that this is the same with the address that ganache list for the first account to make sure the derivation is correct
 const private_key = addr_node.getWallet().getPrivateKey();
 
-const sleep = async function(delay) {await new Promise((r) => setTimeout(r, delay));}
+const sleep = async function(delay) {await new Promise((r) => setTimeout(r, delay));};
 
 function genTx(nonce,gasPrice) {
   const tx = new EthereumTx({
@@ -83,4 +83,86 @@ test('dropped tx', async () => {
   expect(receipt).toBeNull();
   await server.stop();
 },10000);
+
+test('when a TX is delayed or dropped, subsequent TXs are not sent', async () => {
+  const server = new RpcServer({upstream:'http://localhost:2000', host:'0.0.0.0', port:'8554',minGas:2});
+  server.start();
+  await sleep(1000);
+  const web3 = new Web3('http://localhost:8554');
+
+  const nonce = await web3.eth.getTransactionCount(ethFrom);
+  const serializedTx1 = genTx(nonce,1E9);
+  const serializedTx2 = genTx(nonce+1,2E9);
+
+  const hash1 = await new Promise((resolve)=> {
+    web3.eth.sendSignedTransaction(serializedTx1)
+      .once('transactionHash', async hash => {
+        expect(hash).toContain('0x');
+        resolve(hash);
+      })
+  });
+
+  const hash2 = await new Promise((resolve)=> {
+    web3.eth.sendSignedTransaction(serializedTx2)
+      .once('transactionHash', async hash => {
+        expect(hash).toContain('0x');
+        resolve(hash);
+      })
+  });
+
+  await sleep(2000);
+  console.log('check tx receipts');
+  const receipt1 = await web3.eth.getTransactionReceipt(hash1);
+  const receipt2 = await web3.eth.getTransactionReceipt(hash2);
+  expect(receipt1).toBeNull();
+  expect(receipt2).toBeNull();
+  await server.stop();
+},20000);
+
+test('when a TX is delayed, subsequent TXs are not queued and sent once the nonce clears', async () => {
+  const server = new RpcServer({upstream:'http://localhost:2000', host:'0.0.0.0', port:'8554',minGas:2});
+  server.start();
+  await sleep(1000);
+  const web3 = new Web3('http://localhost:8554');
+
+  const nonce = await web3.eth.getTransactionCount(ethFrom);
+  const serializedTx1 = genTx(nonce,1E9);
+  const serializedTx2 = genTx(nonce+1,2E9);
+  const serializedTx3 = genTx(nonce,2E9);
+
+  await new Promise((resolve)=> {
+    web3.eth.sendSignedTransaction(serializedTx1)
+      .once('transactionHash', async hash => {
+        expect(hash).toContain('0x');
+        resolve(hash);
+      })
+  });
+
+  const hash2 = await new Promise((resolve)=> {
+    web3.eth.sendSignedTransaction(serializedTx2)
+      .once('transactionHash', async hash => {
+        expect(hash).toContain('0x');
+        resolve(hash);
+      })
+  });
+
+  await sleep(2000);
+  const hash3 = await new Promise((resolve)=> {
+    web3.eth.sendSignedTransaction(serializedTx3)
+      .once('transactionHash', async hash => {
+        expect(hash).toContain('0x');
+        resolve(hash);
+      })
+  });
+
+  await sleep(2000);
+
+  console.log('check tx receipts');
+  const receipt3 = await web3.eth.getTransactionReceipt(hash3);
+  const receipt2 = await web3.eth.getTransactionReceipt(hash2);
+  expect(receipt3.status).toBeTruthy();
+  expect(receipt2).not.toBeNull();
+  expect(receipt2.status).toBeTruthy();
+  await server.stop();
+},20000);
 
